bbba9197f96f6e55de9687c4ab8f44da98555ddf
shiftsmart <shiftsmart@gmail.com>
2022-06-06 15:10:26 +0800

    update0606

---
+++ unity_SHIFT_2D_Parkout_20220606/Assets/script/cow.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cow : MonoBehaviour
+{
+
+    public float jumpheight = 10;
+    public float speed = 700;
+    public Rigidbody2D body;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+
+            Move(1);
+   
+     
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+
+        }
+    }
+
+    void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, jumpheight);
+
+        anim.SetBool("jump", true);
+        /// anim.SetTrigger("Jump");
+
+    }
+    private void Move(int i)
+    {
+
+        body.velocity = new Vector2(i * speed * Time.deltaTime, body.velocity.y);
+ 
+    }
+
+ 
+}
